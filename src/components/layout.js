f904e92d4b8121f88777bb395c76b478a04d903c
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-24 09:30:26 +0100

    Initial Commit

---
+++ src/components/layout.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Footer from "./footer"
+
+export default ({ children }) => 
+    <div className="container">
+        {children}
+        <Footer/ >
+    </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import Main from "../components/main"
+import Layout from "../components/layout"
+
+export default () => 
+    <Layout>
+        <Main>
+            <Headline />
+            <Portfolio />
+        </Main>
+    </Layout>
+
+
+const Headline = () => 
+    <div className="headline-container">
+        <h1>Hi, I'm Dominik</h1>
+        <h2>Software Developer</h2>
+
+        <p>Welcome to my portfolio website. To see some of my work, click the button below</p>
+        <button type="button" className="view-projects" name="view-projects">VIEW PROJECTS</button>
+    </div>
+
+
+const Portfolio = () =>
+    <div className="portfolio-container">
+        <h1>Projects</h1>
+    </div>
