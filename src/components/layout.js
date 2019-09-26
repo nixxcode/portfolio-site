ece6c8c9d8c6441d0b7c73735e2a1a4a6139639d
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-26 18:48:56 +0100

    Include FontAwesome for icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,9 @@
 import React from "react"
 
 import Footer from "./footer"
+import "../styles/global.css"
+import "../styles/fontawesome/css/fontawesome.css"
+import "../styles/fontawesome/css/solid.css"
 
 export default ({ children }, props) => 
     <div className="container">
