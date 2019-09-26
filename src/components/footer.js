c4e13e2c168ed011f942d16dc909ff20cbdb42ff
Dominik P <46871088+nixxcode@users.noreply.github.com>
2019-09-26 18:49:55 +0100

    Add Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,9 @@ import React from "react"
 export default props => 
     <footer>
         <div className="footer-container">
-            <p>FOOTER text</p>
+            <span><i className="fas fa-envelope"></i><a target="_blank" href="mailto:dominik.petrovic87@gmail.com">dominik.petrovic87@gmail.com</a></span>
+            <span><i className="fas fa-phone-alt"></i>(+44) 07398 234 570</span>
+            <span><i className="fas fa-code-branch"></i><a target="_blank" href="https://github.com/nixxcode">https://github.com/nixxcode</a></span>
+            <span><i className="fas fa-copyright"></i>Dominik Petrovic, 2019</span>
         </div>
     </footer>
